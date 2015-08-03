d15902ae943c4abb80a17bc5a5ff800a0c24e8f1
Jonathan Bird <jquark@gmail.com>
2015-08-03 14:05:36 +0200

    reworked pm_to_velocities, wrote gettrs, likelihood now interacts with data well

--- avrlikelihood.py
+++ avrlikelihood.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy as np
 import pm_to_velocities as pm_to_vels
 
@@ -6,6 +7,7 @@ class HyperParams():
     """
     def __init__(self, sigma2Ws):
         self.age0 = 1.0 #Gyr
+        self.sigma2Ws = sigma2Ws
         self.set_sigma2Ws(sigma2Ws)
 
     def set_sigma2Ws(self, sigma2Ws):
@@ -31,17 +33,19 @@ def lnlh(data, params, hyperparams):
     - This is really brittle, it is only there to serve a single customer
 
     """
-    ages, radii, Ws = data
+    ages = data.get_ages()
+    radii = data.get_radii()
+    Ws = data.get_Ws()
     variance_W0, beta_W, inv_scalelen_W = params
-    variances_W = variance_W0 * (ages/hyperparams.get_age0())**beta_W *\
-            np.exp(-2.*radii*inv_scalelen_W) + hyperparams.get_sigma2Ws()
+    print ('betaW: {}'.format(beta_W))
+    variances_W = (variance_W0 * np.power((ages/hyperparams.get_age0()), beta_W) * np.exp(-2.*radii*inv_scalelen_W) + hyperparams.get_sigma2Ws())
     return -0.5*np.sum(Ws**2/variances_W) -0.5*np.sum(np.log(variances_W))
 
 def lnprior(params, hyperparams):
     """
     Just enforcing constraints (for now)
     """
-    variance_W0, beta_W, scalelen_W = params #you're pathetic
+    variance_W0, beta_W, inv_scalelen_W = params #you're pathetic
     if variance_W0 < 0.:
         return -np.Inf
     if variance_W0 > 10000.:  # (km/s)**2
@@ -50,9 +54,9 @@ def lnprior(params, hyperparams):
         return -np.Inf
     if beta_W > 1.:
         return -np.Inf
-    if inv_scalelen_W < -.1.:# kpc**-1
+    if inv_scalelen_W < -.1 :# kpc**-1
         return -np.Inf
-    if inv_scalelen_W > .1.: # kpc**-1
+    if inv_scalelen_W > .1 : # kpc**-1
         return -np.Inf
     return 0.
 
@@ -66,5 +70,10 @@ def lnprob(data, params, hyperparams):
 if __name__ == "__main__":
     data = pm_to_vels.PMmeasurements(RCcatalog = pm_to_vels.catalog)
     data.to_space_velocties()
-    hyperparams = HyperParams(data.spacevel_err[:,2,2])
-    pass
+    data.UVW_to_galcen()
+    params = (10.,0.4,0.05) #km/s,beta,kpc**-1 
+    #(variance_W0, beta_W, inv_scalelen_W) = params
+    hyperparams = HyperParams(data.get_sigma2Ws())
+    result = lnprob(data,params,hyperparams)
+    print ('lnprob: {}'.format(result))
+    #pass
--- pm_to_velocities.py
+++ pm_to_velocities.py
@@ -1,14 +1,24 @@
-
 import numpy as np
 import galpy
 from galpy.util import bovy_coords as bcoords
-import combinecats
+import utils
 
-catalog = combinecats.returncat()
+catalog = utils.returncat()
 
 class PMmeasurements:
     """
     Class to contain PMs and errors. This will make it each to switch between UCAC and PMXXL later.
+
+    # Bugs
+
+    - galpy:   rewrites the input l,b; need to fork and fix
+        + Hack: change degree keyword to False for conversions (NASTY)
+
+    # Decisions, todos
+
+    - MAJOR Propagate uncertainty tensor from UVW to galactocentric frame
+    - 2015-08-03 Assuming 5% distance errors to calc spacevels.
+    - 2015-08-03 Fork Galpy and rewrite conv to galcencycl coords.
     """
     def __init__(self,pmcatalog='PPMXL', RCcatalog=catalog):
         self.pmcatalog = pmcatalog
@@ -32,10 +42,11 @@ class PMmeasurements:
         assign reasonable variable names to PM columns of RC catalog depending upon which survey is being used. Then pass to Galpy.
         """
         self.data = {}
+        self.data['GALVZ'] = self.catalog[self._colname('GALVZ')][self.mask]
         self.data['pmra'] = self.catalog[self._colname('PMRA')][self.mask]
-        self.data['pmra_err'] = self.catalog[self._colname('PMRA_ERR')][self.mask]
+        self.data['pmra_uncer'] = self.catalog[self._colname('PMRA_ERR')][self.mask]
         self.data['pmdec'] = self.catalog[self._colname('PMDEC')][self.mask]
-        self.data['pmdec_err'] = self.catalog[self._colname('PMDEC_ERR')][self.mask]
+        self.data['pmdec_uncer'] = self.catalog[self._colname('PMDEC_ERR')][self.mask]
         self.data['distance'] = self.catalog['RC_DIST'][self.mask]
         self.data['vlos'] = self.catalog['VHELIO_AVG'][self.mask]
         self.data['RA'] = self.catalog['RA'][self.mask]
@@ -46,53 +57,74 @@ class PMmeasurements:
         self.data['RC_GALZ'] = self.catalog['RC_GALZ'][self.mask]
         self.data['RC_GALPHI'] = self.catalog['RC_GALPHI'][self.mask]
 
-    def covar_pmrapmdec(self):
-        pmrapmdec_corrcoefs = np.corrcoef(self.data['pmra_err'], self.data['pmdec_err'])
-        pmra_err = self.data['pmra_err']
-        pmdec_err = self.data['pmdec_err']
+    def calc_covar_pmrapmdec(self):
+        pmrapmdec_corrcoefs = np.corrcoef(self.data['pmra_uncer'], self.data['pmdec_uncer'])
+        pmra_uncer = self.data['pmra_uncer']
+        pmdec_uncer = self.data['pmdec_uncer']
         ###step 2: propogate corresponding errors
-        self.covar_pmradec = np.array([[pmra_err**2, pmrapmdec_corrcoefs[0,1]*pmra_err*pmdec_err], [pmrapmdec_corrcoefs[1,0]*pmra_err*pmdec_err, pmdec_err**2]]).T  #transpose for shape for galpy
+        self.covar_pmradec = np.array([[pmra_uncer**2, pmrapmdec_corrcoefs[0,1]*pmra_uncer*pmdec_uncer], [pmrapmdec_corrcoefs[1,0]*pmra_uncer*pmdec_uncer, pmdec_uncer**2]]).T  #transpose for shape for galpy
 
-    def pmrapmdec_to_pmllpmbb(self):
+    def conv_pmrapmdec_to_pmllpmbb(self):
         ###step 1: convert PM radec to PM l,b
         self.pmll_pmbb = bcoords.pmrapmdec_to_pmllpmbb(self.data['pmra'], self.data['pmdec'], self.data['RA'],self.data['DEC'], degree=self.degree, epoch=2000.0)
 
+    def calc_covar_pmllpmbb(self):
+        self.covar_pmllpmbb = bcoords.cov_pmrapmdec_to_pmllpmbb(self.covar_pmradec,self.data['RA'], self.data['DEC'], degree=self.degree,epoch=2000.0)
+
+    def calc_spacevel(self):
+        """
+        Calculates U,V,W
+        """
+        self.spacevels = bcoords.vrpmllpmbb_to_vxvyvz(self.data['vlos'],self.pmll_pmbb[:,0]*np.cos(np.radians(self.data['GLAT'])), self.pmll_pmbb[:,1], self.data['GLON'], self.data['GLAT'], self.data['distance'], degree=self.degree)
+
+    def calc_spacevel_uncer_var_tensor(self):
+        """
+        Right now assumes ZERO error in RV (km/s) and 5% distance errors
+        """
+        dist_uncer = 0.05 * self.data['distance']
+        self.spacevel_uncer_var_tensor = bcoords.cov_dvrpmllbb_to_vxyz(self.data['distance'], dist_uncer, np.zeros_like(self.data['vlos']), self.pmll_pmbb[:,0], self.pmll_pmbb[:,1], self.covar_pmllpmbb, self.data['GLON'], self.data['GLAT'], degree=self.degree)
 
     def to_space_velocties(self):
         """
-        Wrapper around galpy, and wrapper to go through steps
+        Wrapper around galpy, and wrapper to go through steps to calc UVW
         """
-        #Build covar_pmrapmdec
-        print (self.data['GLON'][:10], "Two")
-        pmrapmdec_corrcoefs = np.corrcoef(self.data['pmra_err'], self.data['pmdec_err'])
-        #easier to read
-        print (self.data['GLON'][:10], "Three")
-        pmra_err = self.data['pmra_err']
-        pmdec_err = self.data['pmdec_err']
-        ###step 2: propogate corresponding errors
-        covar_pmradec = np.array([[pmra_err**2, pmrapmdec_corrcoefs[0,1]*pmra_err*pmdec_err], [pmrapmdec_corrcoefs[1,0]*pmra_err*pmdec_err, pmdec_err**2]]).T  #transpose for shape for galpy
-        print (self.data['GLON'][:10], "4")
-        self.covar_pmllpmbb = bcoords.cov_pmrapmdec_to_pmllpmbb(covar_pmradec,self.data['RA'], self.data['DEC'], degree=self.degree,epoch=2000.0)
-        #### Make galactocentric X,Y,Z for spacevel calc. Include Zsun
-        print (self.data['GLON'][:10], "5")
-
-        #galXYZ = bcoords.cyl_to_rect(self.data['RC_GALR'], self.data['RC_GALPHI'], self.data['RC_GALZ'])
+        self.conv_pmrapmdec_to_pmllpmbb()
+        self.calc_covar_pmrapmdec()
+        self.calc_covar_pmllpmbb()
         self.XYZ = np.array(bcoords.lbd_to_XYZ(self.data['GLON'], self.data['GLAT'], self.data['distance'], degree=True))
-        ###step 3: convert l,b,d,rv to space velocities
-        ### Replacing l,b,d with galactocentric coords
-        print (self.data['GLON'][:10], "6")
-        self.spacevels = bcoords.vrpmllpmbb_to_vxvyvz(self.data['vlos'],self.pmll_pmbb[:,0]*np.cos(np.radians(self.data['GLAT'])), self.pmll_pmbb[:,1], self.data['GLON'], self.data['GLAT'], self.data['distance'], degree=self.degree)
-        ###step 4: propagate errors as welll
-        ## assume 5% distance errors
-        print (self.data['GLON'][:10], "6")
-        dist_err = 0.05 * self.data['distance']
-        self.spacevel_err = bcoords.cov_dvrpmllbb_to_vxyz(self.data['distance'], dist_err, np.zeros_like(self.data['vlos']), self.pmll_pmbb[:,0], self.pmll_pmbb[:,1], self.covar_pmllpmbb, self.data['GLON'], self.data['GLAT'], degree=self.degree)
-        print (self.data['GLON'][:10], "7")
+        self.calc_spacevel()
+        self.calc_spacevel_uncer_var_tensor()
 
-        self.spacevel_galcen = bcoords.vxvyvz_to_galcencyl(self.spacevels[:,0], self.spacevels[:,1], self.spacevels[:,2], self.data['RC_GALR'], self.data['RC_GALPHI'], self.data['RC_GALZ'], vsun=[-11.1,30.24*8.,7.2], galcen=True)
-        print (self.data['GLON'][:10], "8")
-        self.rectgal = bcoords.sphergal_to_rectgal(self.data['GLON'], self.data['GLAT'], self.data['distance'], self.data['vlos'], self.pmll_pmbb[:,0], self.pmll_pmbb[:,1], degree=False)
+    def UVW_to_galcen(self):
+        """
+        Converts UVW space vels to galactocentric frame
+        Assumes R0=8 kpc, z0 = 0.025 kpc, vsun=[-11.1,30.24*8.,7.2] km/s
+        Uses R, phi, z in kpc from RC catalog
+        This matches JoBo's RC catalog assumptions
 
+        # Output
+
+        vRg, vTg, vZg  # km/s
+
+        """
+        self.vRvTvZ_g = bcoords.vxvyvz_to_galcencyl(self.spacevels[:,0], self.spacevels[:,1], self.spacevels[:,2], self.data['RC_GALR'], self.data['RC_GALPHI'], self.data['RC_GALZ'], vsun=[-11.1,30.24*8.,7.2], galcen=True)
+
+    def get_ages(self):
+        return self.catalog['cannon_AGE'][self.mask]
+
+    def get_Ws(self):
+        return self.data['GALVZ'] #km/s from Jo
+
+    def get_radii(self):
+        return self.data['RC_GALR'] #km/s from Jo
+
+    def get_sigma2Ws(self):
+        return self.spacevel_uncer_var_tensor[:,2,2] #(km.s)**2
+
+###Abandoned, left for dead until further notice    
+#print (self.data['GLON'][:10], "8")
+#self.rectgal = bcoords.sphergal_to_rectgal(self.data['GLON'], self.data['GLAT'], self.data['distance'], self.data['vlos'], self.pmll_pmbb[:,0], self.pmll_pmbb[:,1], degree=False)
+#
 
 
 
