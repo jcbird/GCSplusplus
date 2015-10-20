526772ca3d5e832db7d6ba424fe1151bb18abc5e
Jonathan Bird <jquark@gmail.com>
2015-10-20 17:56:58 -0500

    PEP8ify avrlikelihood.py

--- avrlikelihood.py
+++ avrlikelihood.py
@@ -3,8 +3,6 @@ import numpy as np
 import pm_to_velocities as pm_to_vels
 import emcee
 import triangle
-from collections import namedtuple
-import sys
 
 
 class HyperParams():
@@ -56,6 +54,7 @@ def plot_and_run_emcee(nexec, ndim, nwalkers, p0, lnprob_func, lnprob_args):
             sampler = run_emcee(ndim, nwalkers, p0, lnprob_func, lnprob_args)
             mk_triangle_plot(sampler)
 
+
 def lnlh(data, params, hyperparams):
     """
     # Inputs
@@ -73,16 +72,19 @@ def lnlh(data, params, hyperparams):
     radii = data.radii
     Ws = data.Ws
     variance_W0, beta_W, inv_scalelen_W = params
-    #print ('betaW: {}'.format(beta_W))
-    variances_W = (variance_W0 * np.power(( ages / hyperparams.get_age0()), 2. * beta_W) *
-            np.exp(-2. * radii * inv_scalelen_W) + hyperparams.get_sigma2Ws())
-    return -0.5 * np.sum(Ws * Ws / variances_W) -0.5 * np.sum(np.log(variances_W))
+    variances_W = (variance_W0 * np.power((ages / hyperparams.get_age0()),
+                                          2. * beta_W) *
+                   np.exp(-2. * radii * inv_scalelen_W) +
+                   hyperparams.get_sigma2Ws())
+    return (-0.5 * np.sum(Ws * Ws / variances_W) -
+            0.5 * np.sum(np.log(variances_W)))
+
 
 def lnprior(params, hyperparams):
     """
     Just enforcing constraints (for now)
     """
-    variance_W0, beta_W, inv_scalelen_W = params #you're pathetic
+    variance_W0, beta_W, inv_scalelen_W = params  # you're pathetic
     if variance_W0 < 0.:
         return -np.Inf
     if variance_W0 > 10000.:  # (km/s)**2
@@ -91,11 +93,12 @@ def lnprior(params, hyperparams):
         return -np.Inf
     if beta_W > 1.:
         return -np.Inf
-    if inv_scalelen_W < -.1 :# kpc**-1
+    if inv_scalelen_W < -.1:  # kpc**-1
         return -np.Inf
-    if inv_scalelen_W > .2 : # kpc**-1
+    if inv_scalelen_W > .2:  # kpc**-1
         return -np.Inf
     return 0.
+
 
 def lnprob(params, data, hyperparams):
     lnp = lnprior(params, hyperparams)
@@ -103,34 +106,33 @@ def lnprob(params, data, hyperparams):
         return -np.Inf
     return lnp + lnlh(data, params, hyperparams)
 
+
 def mk_triangle_plot(sampler, nstart=500):
-    samples = sampler.chain[:,nstart:,:].reshape((-1,3))
+    samples = sampler.chain[:, nstart:, :].reshape((-1, 3))
     fig = triangle.corner(samples, labels=[r"$S_{W_0}$ [km$^2$/s$^2$]",
-        r"$\beta$", r"$R_{W_0}^{-1}$ [kpc]"])
+                                           r"$\beta$",
+                                           r"$R_{W_0}^{-1}$ [kpc]"])
     fig.savefig("triangle0.png")
 
 
 if __name__ == "__main__":
-    pmdata = pm_to_vels.PMmeasurements(RCcatalog = pm_to_vels.catalog, pmcatalog='UCAC')#,biascorrect='dgalu')
-    #pmdata = pm_to_vels.PMmeasurements(RCcatalog = pm_to_vels.catalog)#,biascorrect='dgalu')
-    pmdata.height_cut(maxheight = 0.85) #kpc
+    pmdata = pm_to_vels.PMmeasurements(RCcatalog=pm_to_vels.catalog,
+                                       pmcatalog='UCAC')
+    pmdata.height_cut(maxheight=0.85)  # kpc
     pmdata.to_space_velocties()
     pmdata.UVW_to_galcen()
-    data = pmdata.get_tau_radii_vZg_sigma2Ws_container()#kkmax_uncer_variance=100.)
-    #data = pmdata.get_tau_radii_vZg_sigma2Ws_container()#max_uncer_variance=200.)
+    data = pmdata.get_tau_radii_vZg_sigma2Ws_container()
     hyperparams = HyperParams(data.sigma2Ws)
     ndim = 3
     nwalkers = 20
-    init_guess = (225.0, 0.35, 0.06) #km/s,beta,kpc**-1 param guesses
-    p0 = init_emcee(init_guess,nwalkers)
-    plot_and_run_emcee(nexec=1, ndim=ndim, nwalkers=nwalkers, p0=p0, lnprob_func=lnprob, lnprob_args=[data,hyperparams])
-    #emcee_sampler = run_emcee(ndim=ndim, nwalkers=nwalkers, p0=p0, lnprob_func=lnprob, lnprob_args=[data,hyperparams])
-    #mk_triangle_plot(emcee_sampler)
+    init_guess = (225.0, 0.35, 0.06)  # km/s,beta,kpc**-1 param guesses
+    p0 = init_emcee(init_guess, nwalkers)
+    plot_and_run_emcee(nexec=1, ndim=ndim, nwalkers=nwalkers, p0=p0,
+                       lnprob_func=lnprob, lnprob_args=[data, hyperparams])
 
+# #ndim = 3   #HACK, need to fix!!!
+# ###samples = emcee_sampler.chain[:,1000:,:].reshape((-1,ndim))
 
-##ndim = 3   #HACK, need to fix!!!
-####samples = emcee_sampler.chain[:,1000:,:].reshape((-1,ndim))
-
-#result = lnprob(data,params,hyperparams)
-#print ('lnprob: {}'.format(result))
-#pass
+# result = lnprob(data,params,hyperparams)
+# print ('lnprob: {}'.format(result))
+# pass
